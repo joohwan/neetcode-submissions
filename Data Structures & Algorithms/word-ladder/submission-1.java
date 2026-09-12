a19a618fd3caefd93cb8ac473043e926980a99a1
joohwan <joh@hubheadco.com>
2026-09-11 20:02:33 -0600

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> addedToQ = new HashSet<>();
+        Deque<String> q = new ArrayDeque<>();
+        q.addLast(beginWord);
+        addedToQ.add(beginWord);
+        
+        int level = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            level++;
+            for (int i = 0; i < size; i++) {
+                String adj = q.removeFirst();
+                if (adj.equals(endWord)) {
+                    return level;
+                } else {
+                    for (String word : wordList) {
+                        if (!addedToQ.contains(word) && isOneCharDiff(adj, word)) {
+                            q.addLast(word);
+                            addedToQ.add(word);
+                        }
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    boolean isOneCharDiff(String a, String b) {
+        int diffCount = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                diffCount++;
+                if (diffCount > 1) {
+                    return false;
+                }
+            }
+        }
+        return diffCount == 1;
+    }
+}
